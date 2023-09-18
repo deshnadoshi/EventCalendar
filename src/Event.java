5b96c6556ee1f249d494e2125aa18be722e3ae9f
Deshna Doshi <deshna.doshi@gmail.com>
2023-09-18 13:15:01 -0400

    Version 0-2

--- src/Event.java
+++ src/Event.java
@@ -1,7 +1,9 @@
-public class Event {
+public class Event implements Comparable<Event>{
     private Date date; //the event date
     private Timeslot startTime; //the starting time
     private Location location;
     private Contact contact; //include the department name and email
     private int duration; //in minutes
+
+
 }
