2fbd7d4c270023d7e6e43969c8d5f26ba1e0d6cf
Emboar02 <45805212+Emboar02@users.noreply.github.com>
2023-09-21 17:46:09 -0400

    Testing committing

--- src/eventorganizer/EventOrganizer.java
+++ src/eventorganizer/EventOrganizer.java
@@ -3,5 +3,6 @@ package eventorganizer;
 public class EventOrganizer {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("oops");
     }
 }
