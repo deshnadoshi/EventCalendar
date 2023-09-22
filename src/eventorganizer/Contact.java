1f51f4246b340e6631760d0d5259626478b9b725
Deshna Doshi <deshna.doshi@gmail.com>
2023-09-22 11:06:19 -0400

    Updates: Working on Contact.java

--- src/eventorganizer/Contact.java
+++ src/eventorganizer/Contact.java
@@ -3,44 +3,58 @@ package eventorganizer;
 public class Contact {
     private Department department;
     private String email;
-
-    // email: cs@rutgers.edu, bait@rutgers.edu, iti@rutgers.edu
     public Contact(Department department, String email){
         this.department = department;
         this.email = email;
     }
 
     public boolean departmentCheck(){
+        String userInputDepartment = department.toString().toLowerCase();
+        String [] acceptableDeptName = {"cs", "ee" , "iti", "bait", "math"};
 
+        for (int i = 0; i < acceptableDeptName.length; i++){
+            if (acceptableDeptName[i].equals(userInputDepartment)){
+                return true;
+            }
+        }
 
         return false; 
     }
 
     public boolean emailCheck(){
+        email = email.toLowerCase();
 
-        String [] emailArr = email.split("@");
+        String [] emailArr = email.split("@", 0);
         String [] acceptableEmailUsername = {"cs", "ee" , "iti", "bait", "math"};
         String acceptableEmailExtension = "rutgers.edu";
 
-        // Delete print statement later
-        for (int i = 0; i < emailArr.length; i++){
-            System.out.println(emailArr[i] + "\n");
-        }
-
         final int EXPECTED_STR_LENGTH = 2;
-        // String is being split at the '@' symbol, so the expected number of elements in the String array is 2
+        final int USERNAME_INDEX = 0;
+        final int EXTENSION_INDEX = 1;
 
-        // DELETE THIS!
-        System.out.println(emailArr.length + "len");
-
+        boolean acceptableUsername = false;
+        boolean acceptableExtension = false;
 
         if (emailArr.length != EXPECTED_STR_LENGTH){
             return false;
+        } else {
+            for (int i = 0; i < acceptableEmailUsername.length; i++){
+                if (emailArr[USERNAME_INDEX].equals(acceptableEmailUsername[i])){
+                    acceptableUsername = true;
+                }
+            }
+            if (emailArr[EXTENSION_INDEX].equals(acceptableEmailExtension)){
+                acceptableExtension = true;
+            }
         }
 
+        if (acceptableUsername && acceptableExtension){
+            return true;
+        }
 
-        return true;
+        return false;
     }
+
     public boolean isValid(){
 
         if (!emailCheck()){
@@ -56,4 +70,13 @@ public class Contact {
 
         return true;
     }
+
+    // Need to delete this later: using FOR TESITNG ONLY --deshna
+    public static void main(String[] args) {
+        Contact c = new Contact(Department.EE, "cs@rutgers.edu");
+        System.out.println(c.departmentCheck());
+
+    }
 }
+
+// ALSO NEED TO CHECK IF DEPARTMENT AND EMAIL MATCH !!
--- src/eventorganizer/EventOrganizer.java
+++ src/eventorganizer/EventOrganizer.java
@@ -3,9 +3,6 @@ package eventorganizer;
 public class EventOrganizer {
     public static void main(String[] args) {
 
-        // I'm using this class temporarily to test Contact.java -- deshna
-        Contact c = new Contact(Department.EE, "cs@rutgers.edu");
-        c.emailCheck();
 
     }
 }
