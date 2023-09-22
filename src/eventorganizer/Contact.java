6f0fa641f0be601bbd180001f71db3d9ae74d3e3
Deshna Doshi <deshna.doshi@gmail.com>
2023-09-22 07:57:45 -0400

    Updates: Began working on Contact.java

--- src/eventorganizer/Contact.java
+++ src/eventorganizer/Contact.java
@@ -1,5 +1,36 @@
 package eventorganizer;
 
 public class Contact {
-    System.out.println("HI HAEJIN!");
+    private Department department;
+    private String email;
+
+    public Contact(Department department, String email){
+        this.department = department;
+        this.email = email;
+    }
+
+    public boolean departmentCheck(){
+
+        return false;
+    }
+
+    public boolean emailCheck(){
+
+        return false;
+    }
+    public boolean isValid(){
+
+        if (!emailCheck()){
+            System.out.println("Invalid email.");
+            return false;
+        } else if (!departmentCheck()){
+            System.out.println("Invalid department.");
+            return false;
+        } else if (!emailCheck() && !departmentCheck()){
+            System.out.println("Invalid email and invalid department");
+            return false;
+        }
+
+        return true;
+    }
 }
